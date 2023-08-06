e88dfb7242464639de72d10510d624b3762de90a
martamagui <73697174+martamagui@users.noreply.github.com>
2023-08-06 19:04:52 +0200

    SharedPreferences in Activity

--- app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/MainActivity.kt
+++ app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.utad.utadonline_pmdm_ud3_gestiondedatos
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    //Nombre de nuestro almacenamiento de datos
+    private val storageName = "SharedPreferencesStorage"
+    private lateinit var sharedPreferencesStorage: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Obtenemos la instancia de SharedPreferences
+        sharedPreferencesStorage = getSharedPreferences(storageName, Context.MODE_PRIVATE)
     }
 }
