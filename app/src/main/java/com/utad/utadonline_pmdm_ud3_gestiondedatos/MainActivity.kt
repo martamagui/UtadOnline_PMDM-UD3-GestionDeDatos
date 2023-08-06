a0f2ef302ba72693b5615a5919ed1422059371d5
martamagui <73697174+martamagui@users.noreply.github.com>
2023-08-06 21:53:55 +0200

    EncryptedSharedPreferences

--- app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/MainActivity.kt
+++ app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/MainActivity.kt
@@ -4,18 +4,43 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
 class MainActivity : AppCompatActivity() {
 
     //Nombre de nuestro almacenamiento de datos
     private val storageName = "SharedPreferencesStorage"
+    private val storageNameForEncrypted = "SharedPreferencesStorage"
     private lateinit var sharedPreferencesStorage: SharedPreferences
+    private lateinit var encryptedSharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initEncryptedSharedPreferences()
+        initSharedPreferences()
+    }
+
+    private fun initSharedPreferences() {
         //Obtenemos la instancia de SharedPreferences
         sharedPreferencesStorage = getSharedPreferences(storageName, Context.MODE_PRIVATE)
+    }
+
+    private fun initEncryptedSharedPreferences() {
+        //Creamos la MaserKey para encriptar y desencriptar nuestros datos
+        val masterKey = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+
+        //Obtenemos la instancia de EncryptedSharedPreferences
+        encryptedSharedPreferences = EncryptedSharedPreferences.create(
+            storageNameForEncrypted,
+            masterKey,
+            this,
+            //Esquema de encriptado
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            //Esquema de desencriptado
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
     }
 }
