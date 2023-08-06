e0d3bb50131c15a220ff6c52a97512b9c42041f2
martamagui <73697174+martamagui@users.noreply.github.com>
2023-08-06 17:39:16 +0200

    Initial commit, custom theme

---
+++ app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.utad.utadonline_pmdm_ud3_gestiondedatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
