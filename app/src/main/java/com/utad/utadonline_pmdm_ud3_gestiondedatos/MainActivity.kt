9246aa5cf6012003e2b505bde869ad3999b4dee7
martamagui <73697174+martamagui@users.noreply.github.com>
2023-08-12 17:34:14 +0200

    Main Activity update

--- app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/MainActivity.kt
+++ app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.utad.utadonline_pmdm_ud3_gestiondedatos
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
 import com.utad.utadonline_pmdm_ud3_gestiondedatos.databinding.ActivityMainBinding
+import com.utad.utadonline_pmdm_ud3_gestiondedatos.shared_preferences.BasicSharedPreferencesActivity
+import com.utad.utadonline_pmdm_ud3_gestiondedatos.shared_preferences.CleanedSharedPreferencesActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +21,21 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnSharedPreferences.setOnClickListener {
+            navigateToSharedPreferences()
+        }
+        binding.btnSharedPreferencesCleaned.setOnClickListener {
+            navigateToSharedPreferencesCleanedCode()
+        }
+    }
+
+    private fun navigateToSharedPreferencesCleanedCode() {
+        val intent = Intent(this, CleanedSharedPreferencesActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSharedPreferences() {
+        val intent = Intent(this, BasicSharedPreferencesActivity::class.java)
+        startActivity(intent)
     }
 }
