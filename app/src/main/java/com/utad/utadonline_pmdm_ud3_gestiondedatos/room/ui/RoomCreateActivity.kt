cd74bb45ba39ab8dbd2d5c560fb8557f221c53d1
martamagui <73697174+martamagui@users.noreply.github.com>
2023-08-20 18:40:20 +0200

    Update RoomCreateActivity.kt

--- app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/room/ui/RoomCreateActivity.kt
+++ app/src/main/java/com/utad/utadonline_pmdm_ud3_gestiondedatos/room/ui/RoomCreateActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.utad.utadonline_pmdm_ud3_gestiondedatos.databinding.ActivityRoomCreateBinding
 
@@ -24,29 +25,46 @@ class RoomCreateActivity : AppCompatActivity() {
     }
 
     private fun checkIfWeAlreadyHaveThisPermission() {
+        //Mediante Manifest.permission.NuestroPermiso
+        // accedemos al permiso que queremos comprobar
         val externalStoragePermission: String = Manifest.permission.READ_EXTERNAL_STORAGE
+        //El método ContextCompat.checkSelfPermission() nos devuelve el estado
+        //del permiso que pasemos cómo parámetro. Lo guardamos en una variable.
         val permissionStatus = ContextCompat.checkSelfPermission(this, externalStoragePermission)
 
-        // Verificar si ya tenemos permiso
         if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
             // Ya tenemos permiso, podemos realizar la acción que lo necesita.
             showGrantedPermissionMessage()
         } else {
-            // No tenemos permiso, mostramos la solicitud al usuario.
-            requestPermissionLauncher.launch(externalStoragePermission)
+
+            // Debemos comprobar si ya hemos pedido con anterioridad el permiso
+            val shouldRequestPermission = shouldShowRequestPermissionRationale(externalStoragePermission)
+            // Deser así, debemos explicar al usuario porqué es necesario el permiso
+            if (shouldRequestPermission) {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("El permiso es necesario")
+                    .setMessage("Es necesario para poder cargar tu imagen de empleado y completar tu ficha")
+                    .setPositiveButton("Okay") { dialog, which ->
+                        //Si el usuario acepta, pedimos de nuevo los permisos
+                        requestPermissionLauncher.launch(externalStoragePermission)
+                    }
+                    .setNegativeButton("Denegar") { dialog, which -> finish() }
+                    .show()
+            } else {
+                requestPermissionLauncher.launch(externalStoragePermission)
+            }
         }
     }
 
     private val requestPermissionLauncher: ActivityResultLauncher<String> =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
-                Snackbar.make(
-                    binding.root,
-                    "El usuario nos concedió el permiso",
-                    Snackbar.LENGTH_SHORT
-                ).show()
+                //El permiso ha sido concedido, podemos realizar la acción que lo necesitaba
+                Snackbar.make(binding.root, "El usuario nos concedió el permiso", Snackbar.LENGTH_SHORT).show()
             } else {
+                //El permiso ha sido denegado, deberemos restringir el acceso a esta parte de la app
                 Snackbar.make(binding.root, "El usuario lo denegó", Snackbar.LENGTH_SHORT).show()
+                finish()
             }
         }
 
